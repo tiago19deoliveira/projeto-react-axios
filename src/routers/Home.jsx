233188f2e2395334c6446bbca176756178fe7086
tiago19deoliveira <tiago18deoliveira@gmail.com>
2024-05-09 11:10:38 -0300

    estilização e preparo para utilizar rotas com axios

--- src/routers/Home.jsx
+++ src/routers/Home.jsx
@@ -1,8 +1,8 @@
 
-import axios from "axios";
 import { useState,useEffect } from "react"
 import{Link} from "react-router-dom"
 import "./Home.css";
+import blogFetch from "../axios/config";
 
 const Home = () => {
 
@@ -11,7 +11,7 @@ const Home = () => {
 
   const getPosts = async()=>{
     try {
-      const response = await axios.get("https://jsonplaceholder.typicode.com/posts")
+      const response = await blogFetch.get("/posts")
       const data = response.data;
       
       setPosts(data)
@@ -23,18 +23,19 @@ const Home = () => {
 
   useEffect(()=>{
 
-getPosts()
+   getPosts()
 
   },[])
 
-  return( <div>
+  return(
+     <div className="home">
     <h1>Últimos Posts</h1>
     {posts.length === 0 ? (<p>Carregando...</p>): (
       posts.map((post) => (
         <div className="post" key={post.id}>
           <h2>{post.title}</h2>
           <p>{post.body}</p>
-          <Link to={'/posts/$(post.id)'} className="btn">Ler Mais</Link>
+          <Link to={`/posts/$(post.id)`} className="btn">Ler Mais</Link>
         </div>
       ))
     )}
--- src/routers/NewPost.jsx
+++ src/routers/NewPost.jsx
@@ -3,7 +3,32 @@ import React from 'react'
 
 function NewPost() {
   return (
-    <div>NewPost</div>
+    <div className='new post'>
+      <h2>
+        Novo post
+      </h2>
+      <form >
+       <div className='form-control'>
+         <label 
+         htmlFor='title' 
+         type="text" 
+         id="title"
+         placeholder="digite o conteúdo"
+         >
+          Novo Post
+         </label>
+       </div>
+       
+       <div className='form-control'>
+         <label htmlFor='body'> Conteudo </label>
+         <textarea name="body" id="body" placeholder='digite o conteudo '></textarea>
+       </div>
+       
+       <input type="submit" value="Criar post" className='btn' />
+
+      </form>
+      
+      </div>
   )
 }
 
